31d34c7399e0ad6a45bfa6b4c85c7756a56b1456
zeno-citiulo <zenocit02@gmail.com>
2024-11-12 15:35:06 +0100

    build: add base project files and add README with base intructions

---
+++ README.md
@@ -0,0 +1,6 @@
+Esercizio js-variables
+===
+Abstract: variabili con javascript
+## Information
+- Partendo dal codice fornito, svolgere tutte le task richieste in ognuno degli script presenti.
+- Utilizzate i devtools e `console.log` per verificare il risultato.
---
+++ ex-js-variables/02-strings/script.js
@@ -0,0 +1,23 @@
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata name e assegnale il tuo nome.
+// Stampa il valore di nome nella console.
+
+// 2. Tipi primitivi
+// Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+// un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il nome completo
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+
+const fullName = null;
+console.log(fullName); // Output: "Jane Smith"
+
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = null;
+console.log(numberOfLettersInAlphabet); // Output: 26
