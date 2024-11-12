23a3b302a0b10f4d9f3160ad7296e334c603cd4b
zeno-citiulo <zenocit02@gmail.com>
2024-11-12 16:13:13 +0100

    feat: exercise 02 completed

--- ex-js-variables/02-strings/script.js
+++ ex-js-variables/02-strings/script.js
@@ -14,10 +14,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = 'Jane' + ' ' + 'Smith';
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
